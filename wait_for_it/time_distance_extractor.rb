40afe63bb2c0090841e33ef2fb2a716c3ca13bd9
Aravind Gopalakrishnan <pro.aravind@gmail.com>
2023-12-15 10:18:45 +0100

    Wait for it: V2 solved

--- wait_for_it/time_distance_extractor.rb
+++ wait_for_it/time_distance_extractor.rb
@@ -1,16 +1,41 @@
 class TimeDistanceExtractor
   def self.run(input)
-    @input = input
+    new(input).run
+  end
 
-    lines = input.split("\n")
-    time_string = lines[0].delete("Time:").strip
-    distance_string = lines[1].delete("Distance:").strip
+  def self.run_v2(input)
+    new(input).run_v2
+  end
 
+  def initialize(input)
+    @lines = input.split("\n")
+  end
+
+  def run
     times = time_string.split.reject(&:empty?)
     distances = distance_string.split.reject(&:empty?)
 
     times.each_with_object({}).with_index do |(time, result), index|
       result[time.to_i] = distances[index].to_i
     end
+  end
+
+  def run_v2
+    time = time_string.delete(" ")
+    distance = distance_string.delete(" ")
+
+    {time.to_i => distance.to_i}
+  end
+
+  private
+
+  attr_reader :lines
+
+  def time_string
+    @time_string ||= lines[0].delete("Time:").strip
+  end
+
+  def distance_string
+    @distance_string ||= lines[1].delete("Distance:").strip
   end
 end
--- wait_for_it/time_distance_extractor_spec.rb
+++ wait_for_it/time_distance_extractor_spec.rb
@@ -9,9 +9,19 @@ describe TimeDistanceExtractor do
     EOS
   end
 
-  subject { described_class.run(input) }
+  describe ".run" do
+    subject { described_class.run(input) }
 
-  it "returns a hash with time-distance pairs" do
-    expect(subject).to eq({7 => 9, 15 => 40, 30 => 200})
+    it "returns a hash with time-distance pairs" do
+      expect(subject).to eq({7 => 9, 15 => 40, 30 => 200})
+    end
+  end
+
+  describe ".run_v2" do
+    subject { described_class.run_v2(input) }
+
+    it "returns a hash with a time-distance pair" do
+      expect(subject).to eq({71530 => 940200})
+    end
   end
 end
